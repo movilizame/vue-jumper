b9e719ad05676d9193ab9043279b17ede59641ca
Fepi <fpfarina@hotmail.com>
2018-03-07 13:55:43 -0300

    Deep search of input, buttons or textarea / method to call jumper / no more need of ref

--- src/jumper.js
+++ src/jumper.js
@@ -5,21 +5,37 @@
     let isFunction = function (functionToCheck) {
         return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
     }
+
+    let action = function (element) {
+        let target = jumpers[element.id].el;
+        let value = jumpers[element.id].value;
+        if (value && value !== '' && jumpers[value]) {
+            target = jumpers[value].el;
+        }
+        if (!jumpers[element.id].arrowFlag) {
+            if (jumpers[element.id].modifiers.blur) {
+                jumpers[element.id].el.blur();
+            }
+            if (isFunction(target[jumpers[element.id].arg])) {
+                target[jumpers[element.id].arg]();
+            }
+        }
+        if (jumpers[element.id].arrowFlag !== undefined) {
+            jumpers[element.id].arrowFlag = false;
+        }
+    }
+
+    let callAction = function (value) {
+        if (jumpers[value]) {
+            action(jumpers[value].el);
+        } else {
+            console.error('Vue-Jumper: The id provided as an argument does not correspond to any element.');
+        }
+    }
     
     let keyHandler = function (event) {
         if ((event.type.toUpperCase() === 'KEYUP' && event.key.toUpperCase() === 'ENTER') || event.type.toUpperCase() === 'CLICK' || event.type.toUpperCase() === 'CHANGE') {
-            let target = jumpers[this.id].target;
-            if (!jumpers[this.id].arrowFlag) {
-                if (jumpers[this.id].modifiers.blur) {
-                    jumpers[this.id].el.blur();
-                }
-                if (isFunction(target[jumpers[this.id].arg])) {
-                    target[jumpers[this.id].arg]();
-                }
-            }
-            if (jumpers[this.id].arrowFlag !== undefined) {
-                jumpers[this.id].arrowFlag = false;
-            }
+            action(this);
         }
     };
     
@@ -29,14 +45,30 @@
         }
     };
 
-    let jumper = {
-        bind: function (el, binding, node) {
-            jumpers[el.id] = {};
-            jumpers[el.id].name = el.id;
-            jumpers[el.id].value = binding.value;
-            jumpers[el.id].arg = binding.arg;
-            jumpers[el.id].el = el;
-            jumpers[el.id].modifiers = binding.modifiers;
+    let addElementToArray = function (el, binding, node) {
+        jumpers[el.id] = {};
+        jumpers[el.id].name = el.id;
+        jumpers[el.id].value = binding.value;
+        jumpers[el.id].arg = binding.arg;
+        jumpers[el.id].el = el;
+        jumpers[el.id].modifiers = binding.modifiers;
+        if (el.tagName.toUpperCase() === 'DIV' || el.tagName.toUpperCase() === 'SPAN') {
+            let searchInput = el.querySelector('input');
+            if (searchInput) {
+                jumpers[el.id].el = searchInput;
+            } else {
+                let searchTextArea = el.querySelector('textarea');
+                if (searchTextArea) {
+                    jumpers[el.id].el = searchTextArea;
+                } else {
+                    let searchButton = el.querySelector('button');
+                    if (searchButton) {
+                        jumpers[el.id].el = searchButton;
+                    }
+                }
+            }
+        }
+        if (!binding.modifiers.wait) {
             el.addEventListener('keyup', keyHandler, false);
             if (el.tagName.toUpperCase() === 'BUTTON') {
                 el.addEventListener('click', keyHandler, false);
@@ -47,11 +79,19 @@
                 el.addEventListener('click', checkArrows, false);
                 jumpers[el.id].arrowFlag = false;
             }
+        }
+    }
+
+    let jumper = {
+        bind: function (el, binding, node) {
+            if (!node.context.$jump) {
+                node.context.$jump = callAction;
+            }
+            addElementToArray(el, binding, node);
         },
         inserted: function (el, binding, node) {
-            jumpers[el.id].target = node.context.$refs[jumpers[el.id].value];
-            if (!jumpers[el.id].target || jumpers[el.id].target === '') {
-                jumpers[el.id].target = jumpers[el.id].el;
+            if (!jumpers[el.id]) {
+                addElementToArray(el, binding, node);
             }
         },
         unbind: function (el, binding, node) {
