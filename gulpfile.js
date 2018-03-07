332d50673587435e7d018c02a1de9bb81da9a523
Fepi <fpfarina@hotmail.com>
2018-03-07 14:16:42 -0300

    Delete unnecesary modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,12 @@
 
 const gulp = require('gulp');
 const uglify = require('gulp-uglify-es').default;
-
 const babel = require('gulp-babel');
-/* 
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-*/
 
-const files = ['README.md', 'package_publish.json', 'LICENSE'];
+const files = ['README.md', 'src/package.json', 'LICENSE'];
 
 gulp.task('build', ['uglify', 'copyFiles'], function() {
 });
-
-/*
-gulp.task('babelify', function () {
-    console.log('Uglifying and babelify source code ');
-    return browserify('./src/jumper.js')
-        .transform(babelify, {
-            presets: ['es2015'], 
-            plugins: ['add-module-exports']
-        })
-    .bundle()
-    .on('error', function (err) {
-        console.error(err);
-        this.emit('end');
-    })
-    .pipe(source('jumper.js'))
-    .pipe(gulp.dest('./dist'));
-});
-*/
 
 gulp.task('uglify', function () {
     console.log('Uglifying and babelify source code ');
