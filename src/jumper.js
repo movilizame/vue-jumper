959910b8b8a4b62ce57dcbff3a9c453e8e483010
Fepi <fpfarina@hotmail.com>
2018-08-08 12:24:43 -0300

    new version -  auto jump - no more ref

--- src/jumper.js
+++ src/jumper.js
@@ -1,91 +1,201 @@
 (function() {
 
-    let jumpers = {};
-
-    let isFunction = function (functionToCheck) {
-        return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-    }
-
-    let action = function (jumper) {
-        let target = jumper.el;
-        if (jumper.value && jumper.value !== '') {
-            if (jumpers[jumper.value]) {
-                target = jumpers[jumper.value].el;
+    class JumperList {
+        constructor () {
+            this.list = [];
+            this.lastAction = null;
+        }
+    
+        reset () {
+            this.list = [];
+            this.lastAction = null;
+        }
+    
+        next (jumper) {
+            let elementPosition = this.list.findIndex((item) => {
+                return item.name === jumper.name;
+            });
+            if (elementPosition > -1) {
+                if (elementPosition + 1 === this.list.length) {
+                    return this.lastAction ? this.lastAction : false;
+                }
+                return this.list[elementPosition + 1];
+            }
+            return false;
+        }
+    
+        add (jumper) {
+            let jumperPosition = this.list.findIndex((item) => {
+                return item.name === jumper.name;
+            });
+            if (jumperPosition === -1) {
+                this.list.push(jumper);
+            } else {
+                this.list[jumperPosition] = jumper;
             }
         }
-        if (jumper.modifiers.blur) {
-            jumper.el.blur();
-        }
-        if (isFunction(target[jumper.arg])) {
-            target[jumper.arg]();
-        }
-    }
-
-    let callAction = function (elementName) {
-        if (jumpers[elementName]) {
-            action(jumpers[elementName]);
-        } else {
-            console.error('Vue-Jumper: The id provided as an argument does not correspond to any element.');
-        }
-    }
     
-    let keyHandler = function (event) {
-        if ((event.type.toUpperCase() === 'KEYDOWN' && event.key.toUpperCase() === 'ENTER') || event.type.toUpperCase() === 'CLICK' || event.type.toUpperCase() === 'CHANGE') {
-            action(jumpers[this.id]);
+        getByName (name, context) {
+            return this.list.find((item) => {
+                return item.name === context + name;
+            });
         }
-    };
     
-     let addElementToArray = function (el, binding, node) {
-        jumpers[el.id] = {};
-        jumpers[el.id].name = el.id;
-        jumpers[el.id].value = binding.value;
-        jumpers[el.id].arg = binding.arg;
-        jumpers[el.id].el = el;
-        jumpers[el.id].modifiers = binding.modifiers;
-        if (el.tagName.toUpperCase() === 'DIV' || el.tagName.toUpperCase() === 'SPAN') {
-            let searchInput = el.querySelector('input');
-            if (searchInput) {
-                jumpers[el.id].el = searchInput;
+        removeByName (elementId, context) {
+            let jumper = this.getByName(elementId, context);
+            if (jumper) {
+                this.remove(jumper);
             } else {
-                let searchTextArea = el.querySelector('textarea');
-                if (searchTextArea) {
-                    jumpers[el.id].el = searchTextArea;
+                if (this.lastAction && this.lastAction.name === context + elementId) {
+                    this.lastAction = null;
                 } else {
-                    let searchButton = el.querySelector('button');
-                    if (searchButton) {
-                        jumpers[el.id].el = searchButton;
-                    }
+                    console.error('Not found in list. Check:', jumperList);
                 }
             }
         }
-        if (!binding.modifiers.wait) {
-            el.addEventListener('keydown', keyHandler, false);
-            if (el.tagName.toUpperCase() === 'BUTTON') {
-                el.addEventListener('click', keyHandler, false);
+    
+        remove (jumper) {
+            jumper.remove();
+            let jumperPosition = this.list.findIndex((item) => {
+                return item.name === jumper.name;
+            });
+            if (jumperPosition !== -1) {
+                this.list.splice(jumperPosition, 1);
             }
         }
     }
-
+    
+    let isFunction = function (functionToCheck) {
+        return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+    };
+    
+    class Jumper {
+        constructor (el, binding, node, jumperList) {
+            this.name = node.context._uid + el.id;
+            this.value = binding.value;
+            this.arg = binding.arg;
+            this.el = el;
+            this.modifiers = binding.modifiers;
+            this.jumperList = jumperList;
+            this.context = node.context._uid;
+            if (!binding.modifiers.wait) {
+                el.addEventListener('keydown', this.keyHandler.bind(this), false);
+                if (el.tagName.toUpperCase() === 'BUTTON') {
+                    el.addEventListener('click', this.keyHandler.bind(this), false);
+                }
+            }
+            if (binding.arg && binding.arg === 'last') {
+                this.jumperList.lastAction = this;
+            } else {
+                this.jumperList.add(this);
+            }
+        }
+    
+        remove () {
+            if (!this.modifiers.wait) {
+                this.el.removeEventListener('keydown', this.keyHandler, false);
+                if (this.el.tagName.toUpperCase() === 'BUTTON') {
+                    this.el.removeEventListener('click', this.keyHandler, false);
+                }
+            }
+        }
+    
+        keyHandler (event) {
+            if ((event.type.toUpperCase() === 'KEYDOWN' && event.key.toUpperCase() === 'ENTER') || event.type.toUpperCase() === 'CLICK' || event.type.toUpperCase() === 'CHANGE') {
+                this.do();
+            }
+        };
+    
+        do () {
+            let target = this.el;
+            if (this.value && this.value !== '') {
+                let existElement = this.jumperList.getByName(this.value, this.context);
+                if (existElement) {
+                    target = existElement.el;
+                }
+            } else { // custom behaviour -> it's when theres's not a value
+                if (this.el.tagName.toUpperCase() === 'INPUT' || this.el.tagName.toUpperCase() === 'TEXTAREA') { // Si el elemento es un botón, textarea o un input
+                    let next = this.jumperList.next(this);
+                    if (next) {
+                        if (next.el.tagName.toUpperCase() === 'BUTTON') {
+                            this.el.blur();
+                            next.el.click();
+                        } else {
+                            next.el.focus();
+                        }
+                    };
+                }
+            }
+            if (this.modifiers.blur) {
+                this.el.blur();
+            }
+            if (isFunction(target[this.arg])) {
+                target[this.arg]();
+            }
+        }
+    
+        get el () {
+            return this._el;
+        }
+    
+        set el (element) {
+            if (element.tagName.toUpperCase() === 'INPUT' || element.tagName.toUpperCase() === 'TEXTAREA' || element.tagName.toUpperCase() === 'BUTTON') { // Si el elemento es un botón, textarea o un input
+                this._el = element;
+            } else if (element.tagName.toUpperCase() === 'DIV' || element.tagName.toUpperCase() === 'SPAN') { // Si el elemento es un div o un span busco el primer input en su interior
+                let searchInput = element.querySelector('input');
+                if (searchInput) {
+                    this._el = searchInput;
+                } else {
+                    let searchTextArea = element.querySelector('textarea');
+                    if (searchTextArea) {
+                        this._el = searchTextArea;
+                    } else {
+                        let searchButton = element.querySelector('button');
+                        if (searchButton) {
+                            this._el = searchButton;
+                        }
+                    }
+                }
+            } else { // Si el elemento es un div
+                this._el = element;
+            }
+        }
+    }
+    
+    let callAction = function (context, jumperList) {
+        return (elementName) => {
+            let jumper = jumperList.getByName(elementName, context);
+            if (jumper) {
+                jumper.do();
+            } else {
+                console.error('Vue-Jumper: The id provided as an argument does not correspond to any element.');
+            }
+        };
+    };
+    
+    let jumperList = new JumperList();
+    
     let jumper = {
         bind: function (el, binding, node) {
-            if (!node.context.$jump) {
-                node.context.$jump = callAction;
+            if (!node.context.$jump) { // Si no existe en el contexto la función $jump -> la agregamos
+                node.context.$jump = callAction(node.context._uid, jumperList);
             }
-            addElementToArray(el, binding, node);
         },
         inserted: function (el, binding, node) {
-            if (!jumpers[el.id]) {
-                addElementToArray(el, binding, node);
+            if (binding.value !== true) {
+                if (jumperList.list.length === 0 && el.focus) {
+                    el.focus();
+                }
+                new Jumper(el, binding, node, jumperList); // eslint-disable-line no-new
             }
         },
         unbind: function (el, binding, node) {
-            el.removeEventListener('keyup', keyHandler, false);
-            if (el.tagName === 'button') {
-                el.removeEventListener('click', keyHandler, false);
+            if (binding.value !== true) {
+                jumperList.removeByName(el.id, node.context._uid);
             }
-            jumpers[el.id] = undefined;
         }
     };
+    
     
     try {
         module.exports = jumper;
